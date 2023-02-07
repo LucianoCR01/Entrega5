d0cffa15b0080a47f9b4d9b6c456cc02ce683189
Crucci Luciano <crucciluciano@gmail.com>
2023-02-07 13:54:19 -0300

    cinco

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,29 @@ const socket = io.connect();
 const input = document.querySelector('#ell2')
 document.querySelector('#ell').addEventListener('click', () => {
     socket.emit('idEliminar', input.value)
-    console.log(input)
 })
 
 socket.on('messages', data => {
     document.getElementById('lista').innerText = data;
 })
 
-const funciona = document.querySelector('#botAdd2')
+function formulario(){
+    let objProducto = {}
+    const title = document.querySelector('#title')
+    const descripction = document.querySelector('#descripction')
+    const price = document.querySelector('#price')
+    const code = document.querySelector('#code')
+    const stock = document.querySelector('#stock')
+    objProducto.title = title
+    objProducto.descripction = descripction
+    objProducto.price = price
+    objProducto.code = code
+    objProducto.stock = stock
+    console.log(objProducto)
+    return objProducto 
+}
+
+
 document.querySelector("#botAdd").addEventListener('click', () => {
-    socket.emit('addProd', funciona.value)
-    console.log(funciona)
+    socket.emit('addProd', formulario())
 })
